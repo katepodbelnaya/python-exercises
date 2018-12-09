2d77a93064d65449117852247271c12823399eb1
katepodbelnaya <38401987+katepodbelnaya@users.noreply.github.com>
2018-12-09 09:49:19 -0500

    Update cows_and_bulls.py

--- cows_and_bulls.py
+++ cows_and_bulls.py
@@ -10,33 +10,39 @@ def users_number(num):
             print ("It's not 4-digit number. Please, try again.")
     return num
 
-def count_cows_bulls (list1, list2):
+def count_cows (list1, list2):
     c = 0
-    b = 0
     for i in range(4):
         if (list1[i] == list2[i]):
             c += 1
-        elif (list1[i] != list2[i]) and (list2[i] in list1):
+            list1[i] = 11
+            list2[i] = 22
+    return c
+
+def count_bulls (list1, list2):
+    b = 0
+    for i in range(4):
+        if list1[i] in list2:
             b += 1
-    return c, b
+    return b
 
 if __name__=="__main__":
     number = randint(1000,9999)
-    list_of_num = list_of_numbers(number)
     attempts = 0
     cows = 0
-    print (number)
-    
-    
+   # print (number)
+
+
     while cows != 4:
         user_num = 0
-        user_num = users_number(user_num)  
+        user_num = users_number(user_num)
         list_of_user_num = list_of_numbers(user_num)
-        cows, bulls = count_cows_bulls (list_of_num, list_of_user_num)
+        list_of_num = list_of_numbers(number)
+        cows = count_cows (list_of_num, list_of_user_num)
+        bulls = count_bulls (list_of_num, list_of_user_num)
         print ('Cows:', cows)
+        print (list_of_num, list_of_user_num)
         print ('Bulls:', bulls)
         attempts +=1
-        
+
     print ("You've guessed the number! You had {} attempts".format(attempts))
-    
-    
