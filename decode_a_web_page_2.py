0ea7759ba29b6d37069291f5ba25830e352bb8aa
katepodbelnaya <38401987+katepodbelnaya@users.noreply.github.com>
2018-12-11 13:49:55 -0500

    Create decode_a_web_page_2.py

---
+++ decode_a_web_page_2.py
@@ -0,0 +1,33 @@
+#Using the requests and BeautifulSoup Python libraries, print to the screen the full text of the article on this website:
+#http://www.washingtonpost.com/wp-dyn/content/article/2010/08/29/AR2010082902749.html?noredirect=on
+#The article is long, so it is split up between 4 pages.
+#Your task is to print out the text to the screen so that you can read the full article without having to click any buttons.
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+
+url = 'http://www.washingtonpost.com/wp-dyn/content/article/2010/08/29/AR2010082902749.html?noredirect=on'
+flag = True
+
+while flag == True:
+    
+    r = requests.get(url)
+    r_html = r.text
+
+    soup = BeautifulSoup(r_html, "lxml")
+
+    n = 0
+    for tag in soup.find_all("a"):
+        if tag.string == 'Next':
+            #print(tag.get('href'))
+            url = tag.get('href')
+            n = 1
+
+    for tag in soup.find_all("p"):
+        print(tag.string)
+        
+    if n == 0:
+        flag = False
