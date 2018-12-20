1e5623936f54af1c138bca8ab3c3578144f3e730
katepodbelnaya <38401987+katepodbelnaya@users.noreply.github.com>
2018-12-20 12:34:18 -0500

    Update read_from_file.py

--- read_from_file.py
+++ read_from_file.py
@@ -1,7 +1,7 @@
 #Given a .txt file that has a list of a bunch of names, count how many of each name there are in the file,
-#and print out the results to the screen. I have a .txt file for you, if you want to use it!
+#and print out the results to the screen. I have a nameslist.txt file for you, if you want to use it!
 #Extra:
-#Instead of using the .txt file from above (or instead of, if you want the challenge), take this .txt file,
+#Instead of using the .txt file from above (or instead of, if you want the challenge), take this Training_01.txt file,
 #and count how many of each “category” of each image there are. This text file is actually a list 
 #of files corresponding to the SUN database scene recognition database, and lists the file directory hierarchy for the images.
 #Once you take a look at the first line or two of the file, it will be clear which part represents the scene category.
