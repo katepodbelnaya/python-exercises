0350aa14260f23c6b90ba8d0184c7a8e9fa3c35d
katepodbelnaya <38401987+katepodbelnaya@users.noreply.github.com>
2019-04-16 21:14:07 -0400

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,26 +1,27 @@
-def search1 (lis, num):
-    if num in lis:
-        print ('The number is in the list')
-        
 def binary_search (lis, num):
-    newlist = []
-    if num == lis[int(len(lis)/2)]:
-        print ('The number is in the list')
-    elif num > lis[int(len(lis)/2)]:
-        for i in range (int(len(lis)/2), len(lis)):
-            newlist.append(lis [i])
-    else:
-        for i in range (int(len(lis)/2)):
-            newlist.append(lis[i])
-    if len(newlist) > 0 and num < lis [len(lis) -1]:
-        binary_search(newlist, num)
-    else:
-        print ('The number is not in the list')
     
+    st = 'The number is not in the list'
+    
+    if len(lis) == 0:
+        print (st)
+
+    left, right = 0, len(lis) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if lis[mid] == num:
+            st = 'The number is in the list'
+            break
+        elif lis[mid] < num:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    # End Condition: left > right
+    print (st)
+    
+
+    
+
 list1 = [1, 5, 16, 20, 56, 100, 267, 300]
-number = 2000
 
-binary_search(list1, number)
-
-#search1(list1, number)
-
+number = 9
