bb58161bbcb4ea35d6fc3efb5f772cb792e80508
katepodbelnaya <38401987+katepodbelnaya@users.noreply.github.com>
2019-04-16 21:40:26 -0400

    Update sqrt.py
    Add doc string

--- sqrt.py
+++ sqrt.py
@@ -1,4 +1,9 @@
 def mySqrt(x):
+    """
+    Implement int sqrt(int x).
+    Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+    Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+    """
     if x == 0:
         return 0
     
